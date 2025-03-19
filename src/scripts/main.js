d0460466e00b1a2de6142c1dde3668a1ac439968
samuel <samuel@teste.com>
2025-03-18 21:06:52 -0300

    contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,33 @@
-console.log("olá mundo");
+AOS.init();
+
+const dataDoEvento = new Date ("Sep 13, 2025 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaMs = 1000 * 60 * 60 * 24;
+    const horaMs = 1000 * 60 * 60;
+    const minMs = 1000 * 60;
+
+    const diasAteEvento = Math.floor(distanciaAteEvento / diaMs);
+    const horasAteEvento = Math.floor((distanciaAteEvento % diaMs) / horaMs);
+    const minutosAteEvento = Math.floor((distanciaAteEvento % horaMs) / minMs);
+    const segundosAteEvento = Math.floor((distanciaAteEvento % minMs) / 1000);
+
+    console.log(diasAteEvento);
+    console.log(horasAteEvento);
+    console.log(minutosAteEvento);
+    console.log(segundosAteEvento);
+
+    document.getElementById('contador').innerHTML = `${diasAteEvento}d ${horasAteEvento}h ${minutosAteEvento}m ${segundosAteEvento}s`;
+
+    if (distanciaAteEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'Evento expirado!'
+    };
+
+}, 1000);
